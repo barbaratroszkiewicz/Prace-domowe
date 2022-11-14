293e22b900ae83cb17fc53436d646698a9ce97e0
barbaratroszkiewicz <115422976+barbaratroszkiewicz@users.noreply.github.com>
2022-11-14 23:28:34 +0100

    Add files via upload

---
+++ pracadomowa1.py
@@ -0,0 +1,13 @@
+
+if __name__ == '__main__':
+    list1 = [ 'dog' , 'cat' , 'mouse' , 'fish']
+    list2 = [ 'flower' , 'stone' , 'stick' , 'leaf']
+    twolists = [list1 , list2]
+    for list in (twolists):
+        print(twolists[0][0] , twolists[1][0] , twolists[0][1] , twolists[1][1] )
+
+# Proszę o pomoc jak to zrobić prościej bo tylko do tego udało mi się dojść :(
+
+
+
+
